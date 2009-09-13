c5ccb5d35b52140d91347364abf29f8ab0de375c
Richard Hubers <richard.hubers@gmail.com>
2009-09-13 13:34:19 +0200

    Added UsageAttribute.

---
+++ src/Optional/Attributes/UsageAttribute.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Optional.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property, AllowMultiple = false, Inherited = false)]
+    public class UsageAttribute : ValueAttribute
+    {
+        public UsageAttribute(string value)
+            : base(value)
+        {
+        }
+    }
+}
